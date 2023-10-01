fa459b401ef5306327fe3ab822397d75a6f84a4a
Sanjay Puri <purisanjay007@gmail.com>
2023-10-01 17:49:54 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w3-container">
+      <div className="w3-panel w3-pale-blue w3-monospace" style={{fontSize:'30px'}}>
+        Share Trading App comming up soon....
+      </div>
     </div>
   );
 }
